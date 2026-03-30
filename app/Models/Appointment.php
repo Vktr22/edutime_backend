0505f7714c096d86dc3de8dbccfdba5df0595d02
Vktr22 <nds.viktoria@gmail.com>
2026-03-30 03:22:04 +0200

    letreh appointments table ->migrations+models (id+Tid+Sid+LT)

---
+++ app/Models/Appointment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    
+    protected $fillable = [
+        'teacher_id',
+        'student_id',
+        'lesson_time',
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+}
